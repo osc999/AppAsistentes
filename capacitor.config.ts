205b812e8a7c3effbdc99ebc306dc8d9529638f6
oscar clavijo <os.clavijo@duocuc.cl>
2024-09-07 20:54:48 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppAsistente',
+  webDir: 'www'
+};
+
+export default config;
